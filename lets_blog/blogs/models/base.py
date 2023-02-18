d571f2c52a3040bbc0de29cd823f43a85f673b2d
Kumar Karan <kk.engineer@gmail.com>
2023-02-18 17:12:58 +0530

    Add JWT

--- lets_blog/blogs/models/base.py
+++ lets_blog/blogs/models/base.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 class TimeStamp(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateField(auto_now=True, blank=True)
+
+    class Meta:
+        abstract = True
